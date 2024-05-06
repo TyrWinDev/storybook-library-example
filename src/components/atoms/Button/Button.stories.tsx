4d0d9f6ea7939c24c8919f5f02434cbc5ddb4d85
TyrWinDev <tyrwinterhd@gmail.com>
2024-05-06 13:55:37 -0400

    chore: Add Button component variants and styles
    ```

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -14,9 +14,23 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const  Solid: Story = {
     args: {
+        variant: "solid",
         children: "Click me",
-        
+    }
+}
+
+export const Outline: Story ={
+    args: {
+        variant: "outline",
+        children: "Click me",
+    }
+}
+
+export const Ghost: Story = {
+    args: {
+        variant: "ghost",
+        children: "Click me",
     }
 }
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,12 +1,60 @@
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
+import {cva, VariantProps} from "class-variance-authority";
+import { cn } from "@/utils";
 
 
-type ButtonProps = ComponentProps<"button">;
+const buttonStyles = cva([
+  "w-full", "rounded-md", "font-semibold", "focus:outline-none", "disabled:cursor-not-allowed"
+],{
+  variants: {
+    variant: {
+      solid: "",
+      outline: "border-2",
+      ghost: "transition-colors duration-300"
+    },
+    size: {
+      sm: "px-4 py-2 text-sm",
+      md: "px-4 py-2 text-base",
+      lg: "px-6 py-3 text-lg"
+    },
+    colorScheme: {
+      primary: "text-white"
+    }
+  },
+  compoundVariants: [
+    {
+      variant: "solid",
+      colorScheme: "primary",
+      className: "bg-primary-500 hover:bg-primary-600",
+    },
+    {
+      variant: "outline",
+      colorScheme: "primary",
+      className: "text-primary-500 border-primary-500 hover:bg-primary-500 hover:text-white",
+    },
+    {
+      variant: "ghost",
+      colorScheme: "primary",
+      className: "text-primary-500 hover:bg-primary-100",
+  }
+],
+defaultVariants: {
+  variant: "solid",
+  size: "md",
+  colorScheme: "primary"
+},
+}
+)
 
-const Button = ({ ...props }: ButtonProps) => {
+type ButtonProps = ComponentProps<"button"> & VariantProps<typeof buttonStyles>;
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({ variant, size, colorScheme, className, ...props }, ref)  => {
   return (
-    <button className="btn btn-secondary text-blue-500" { ...props }>Click me</button>
+    <button 
+    ref={ref} 
+    className={cn(buttonStyles({variant, size, colorScheme, className}))} { ...props }>Click me</button>
   )
 }
+)
 
 export default Button
